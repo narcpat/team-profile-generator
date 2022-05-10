3f64f83e0bb6577ae3d973cf669fe490a8019422
narcpat <narcissearts@gmail.com>
2022-05-09 22:06:41 -0400

    further added to inquerer prompts

--- index.js
+++ index.js
@@ -3,42 +3,52 @@ const inquirer = require("inquirer");
 const generatePage = require("./src/page-template");
 
 const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name? (Required)",
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log("Please enter your name!");
-          return false;
-        }
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name? (Required)",
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter your name!");
+            return false;
+          }
+        },
       },
-    },
-    {
-      type: "input",
-      name: "employeeId",
-      message: "Please enter your Employee Id!",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Please enter your email address!",
-    },
-    {
-      type: "input",
-      name: "officeNumber",
-      message: "Please enter your work phone number!",
-    },
-    {
-      type: "list",
-      name: "confirmEmployee",
-      message: "Would you like to add an Employee or an Intern?",
-      choices: ["Employee", new inquirer.Separator(), "Intern"],
-    },
-  ]);
+      {
+        type: "input",
+        name: "employeeId",
+        message: "Please enter your Employee Id!",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address!",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter your work phone number!",
+      },
+      {
+        type: "list",
+        name: "confirmEmployee",
+        message: "Would you like to add an Employee or an Intern?",
+        choices: ["Employee", "Intern"],
+      },
+    ])
+    .then(response => {
+      console.log(response);
+      if (response.type === "Employee") {
+        engineerQuestions();
+      } else if (response.type === "Intern") {
+        internQuestions();
+      } else {
+      }
+    });
 };
 
 const promptAddEngineer = employeeData => {
@@ -84,6 +94,57 @@ Add an Employee
           return false;
         }
       },
+    },
+    {
+      type: "confirm",
+      name: "confirmAddEngineer",
+      message: "Would you like to enter another Employee?",
+      default: false,
+    },
+  ]);
+};
+
+const promptAddIntern = internData => {
+  console.log(`
+=================
+Add an Intern
+=================
+`);
+
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the name of your intern? (Required)",
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("You need to enter your intern's name!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "employeeId",
+      message: "Please enter their Employee Id!",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please enter their email address!",
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "Enter the School attended by the intern!",
+    },
+    {
+      type: "confirm",
+      name: "confirmAddIntern",
+      message: "Would you like to add another intern?",
+      default: false,
     },
   ]);
 };
