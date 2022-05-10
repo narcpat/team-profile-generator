4454221e3bc3804bce8a36339935cd7eaa1777bb
narcpat <narcissearts@gmail.com>
2022-05-09 20:14:22 -0400

    fixed Employee test. Ran successfully!

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,7 @@
 const Employee = require("../lib/Employee");
 
 test("creates an Employee object", () => {
-  const employee = new Employee("Ali");
+  const employee = new Employee("Ali", "54321", "ali@bootcamp.com");
 
   expect(employee.name).toBe("Ali");
   expect(employee.employeeId).toBe("54321");
